c7bb34b06af8b04ebcf79bb3d1ba4d353874c53c
Boluwatife Olaniyan <42306524+devkijified@users.noreply.github.com>
2026-08-05 03:30:23 +0100

    Update helpers.ts

--- lib/utils/helpers.ts
+++ lib/utils/helpers.ts
@@ -1,1 +1,36 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function formatPrice(price: number) {
+  return new Intl.NumberFormat('en-NG', {
+    style: 'currency',
+    currency: 'NGN',
+    minimumFractionDigits: 0,
+  }).format(price)
+}
+
+export function formatDate(date: string) {
+  return new Intl.DateTimeFormat('en-NG', {
+    day: 'numeric',
+    month: 'short',
+    year: 'numeric',
+  }).format(new Date(date))
+}
+
+export function truncateText(text: string, length: number) {
+  if (text.length <= length) return text
+  return text.slice(0, length) + '...'
+}
+
+export function getInitials(name: string) {
+  return name
+    .split(' ')
+    .map(word => word[0])
+    .join('')
+    .toUpperCase()
+    .slice(0, 2)
+}
